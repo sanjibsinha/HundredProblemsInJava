e289c318c9d58b8a1b78351fc995774fbd4d5537
ss <sanjib12sinha@gmail.com>
2019-11-10 09:06:04 +0530

    First Commit

---
+++ src/fun/sanjibsinha/AddingTwoNumbers.java
@@ -0,0 +1,18 @@
+package fun.sanjibsinha;
+import java.util.*;
+public class AddingTwoNumbers {
+
+    public static void main(String[] args){
+        int x, y, sum = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input any number: ");
+        x = in.nextInt();
+        System.out.println("Input another number: ");
+        y = in.nextInt();
+        sum = x + y;
+        System.out.println("Sum of two numbers is : " + sum);
+
+    }
+
+
+}
---
+++ src/fun/sanjibsinha/NewClass.java
@@ -0,0 +1,8 @@
+package fun.sanjibsinha;
+
+public class NewClass {
+    public static void main(String[] args){
+        System.out.println("A new class");
+
+    }
+}
