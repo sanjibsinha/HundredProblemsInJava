43661dd95b958b1811e30dd47cd220b131ef12c8
root <sanjib12sinha@gmail.com>
2019-11-26 16:20:40 +0530

    second commit

---
+++ src/fun/sanjibsinha/ProblemNineteen.java
@@ -0,0 +1,15 @@
+package fun.sanjibsinha;
+
+public class ProblrmNineteen {
+    //local variable
+    public void WeightOfFatherBear(){
+        int weight = 0;
+        weight = 180;
+        System.out.println("The weight of father bear is: " + weight);
+    }
+
+    public static void main(String[] args) {
+        ProblrmNineteen fatherWeight = new ProblrmNineteen();
+        fatherWeight.WeightOfFatherBear();
+    }
+}
